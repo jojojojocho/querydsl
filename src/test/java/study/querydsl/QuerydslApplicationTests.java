80b62ac93f88a7593cd73b0295f690322e6724ed
jo <uuu9214@naver.com>
2022-09-14 01:15:07 +0900

    yml 파일까지 환경설정 완료

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,34 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Object result = query.selectFrom(qHello)
+				.fetchOne();
+	}
+
+}
